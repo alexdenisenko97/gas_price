e7e6efd14a2d4d1fc5cbde1bebb58449e1de6eb3
Alex <134213871+alexdenisenko97@users.noreply.github.com>
2023-05-21 17:23:38 +0300

    Create gas_price_tracker.py

---
+++ gas_price_tracker.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from datetime import datetime
+
+def get_gas_price():
+    response = requests.get('https://www.gasnow.org/api/v3/gas/price')
+    data = json.loads(response.text)
+    gas_price = data['data']['standard']
+    return gas_price
+
+def save_gas_price_to_github(gas_price):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open('gas_price_history.txt', 'a') as file:
+        file.write(f'{timestamp}: {gas_price} Gwei\n')
+
+gas_price = get_gas_price()
+save_gas_price_to_github(gas_price)
